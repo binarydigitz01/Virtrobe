e4406c43efc6dece95e4e77a48dece45a626a5ee
binarydigitz01 <binarydigitz01@protonmail.com>
2024-10-16 18:50:53 +0530

    Registation stuff

--- Android/app/src/main/java/com/manipalhash/virtrobe/MainActivity.java
+++ Android/app/src/main/java/com/manipalhash/virtrobe/MainActivity.java
@@ -1,24 +1,39 @@
 package com.manipalhash.virtrobe;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.databinding.DataBindingUtil;
+
+import com.manipalhash.virtrobe.databinding.ActivityMainBinding;
+import com.manipalhash.virtrobe.ui.register.AccountActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        binding.startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AccountActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
